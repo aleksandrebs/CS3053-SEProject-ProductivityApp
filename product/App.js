fd9418530b9fe1c6314c4f0c247cad071de42cc3
Aleksandre Bit-Suleiman <a.bitsuleiman@gmail.com>
2026-09-22 14:12:57 +0200

    Commit test

--- product/App.js
+++ product/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Log into your account</Text>
       <StatusBar style="auto" />
+      <Button title="Button"/>
     </View>
   );
 }
